132caef5e813866e500892446254fd5801443188
Chhandanghshi-Ghosh <73993378+Chhandanghshi-Ghosh@users.noreply.github.com>
2021-04-01 15:46:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 videoImage=""
+status=""
+sliderV= ""
 
 function preload(){
     videoImage= createVideo('video.mp4')
+    videoImage.hide()
+
 }
 
 function setup(){
-canvas= createCanvas(380,350)
-canvas.center()
+canvas= createCanvas(300,300)
+canvas.position(500,230)
 
-videoImage.hide()
 }
 
 function draw(){
-    image(videoImage, 0 , 0 , 380, 350)
+    image(videoImage, 0 , 0 , 300, 300)
 }
 
 function start(){
 objectDetector= ml5.objectDetector('cocossd',modelLoaded)
-document.getElementById("status").innerHTML="Status: Detection Objects."
+document.getElementById("status").innerHTML="Status: Detecting Objects."
+}
+
+function sliDer(){
+    sliderV=document.getElementById("slider").value
+    videoImage.volume(sliderV)
 }
 
 function modelLoaded(){
 console.log("Model Loaded!!")
 status= "true"
-video.loop()
-video.rate(1)
-video.volume(0)
+videoImage.loop()
+videoImage.speed(1)
+videoImage.volume(0)
 }
+
+
 
